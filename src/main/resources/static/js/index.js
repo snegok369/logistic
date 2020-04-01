606e3c1e09fbf2ab35a8029a073f2f1a9d3315d4
Sneg <snegok369>
2020-04-02 00:02:29 +0300

    Initial commit

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,13 @@
+
+webix.ui({
+    id: 'root',
+    height: 111,
+    rows:[
+        { template:"епта" },
+        { template:"епта" },
+        { template:"епта" },
+        { template:"епта" },
+        { template:"народ" }
+
+    ]
+});
